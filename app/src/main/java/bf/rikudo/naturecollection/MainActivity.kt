e257dbfde3974ec697bcaec0e00761c96b3b811b
rikudosama <bjean633@gmail.com>
2021-04-27 04:56:42 +0000

    correcting the colors files and image importation

--- app/src/main/java/bf/rikudo/naturecollection/MainActivity.kt
+++ app/src/main/java/bf/rikudo/naturecollection/MainActivity.kt
@@ -2,10 +2,17 @@ package bf.rikudo.naturecollection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import bf.rikudo.naturecollection.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // injecter le fragment dans notre boite (fragment_container)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
